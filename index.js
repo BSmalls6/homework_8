13347d7c472fa521a05a3edb0d4409e743dcb88d
BSmalls6 <smallwoodbrennan@gmail.com>
2019-11-13 18:38:21 -0800

    adding github call functions, with accurate id and secret

--- index.js
+++ index.js
@@ -3,46 +3,44 @@ const inquirer = require("inquirer");
 // const github = new Github;
 async function getUsername() {
     try {
-        
-    const username = await inquirer.prompt({
-        message: "What is the Username you wish to search?",
-        name: "Username"
 
-        
-    });
-    user = Object.values(username)
-console.log(Object.values(user));
-}
-    catch (err){
-        console.log (err);
+        const username = await inquirer.prompt({
+            message: "What is the Username you wish to search?",
+            name: "Username"
+
+
+        });
+        user = Object.values(username)
+        let userN = JSON.stringify(user);
+        console.log(userN);
+    }
+    catch (err) {
+        console.log(err);
     }
 };
-
-github.getUser(user)
-.then(data => {
-  console.log(data);
-  if(data.profile.message === 'Not Found') {
-      // Show Alert that user is not Found
-      // This will happen through ui class (ui.js)
-      ui.showAlert('User not found!', 'alert alert-danger');
-
-  } else {
-      // Show Profile
-      // This will happen through ui class (ui.js)
-      ui.showProfile(data.profile);
-
-      // Show repositories
-      ui.showRepos(data.repos);
-  }
-})
-
-} else {
-// Clear the profile
-// This will happen through ui class (ui.js)
-ui.clearProfile();
-}
-})
-
-
 getUsername();
+
+function getUserdata() {
+    github.getUser(user)
+        .then(data => {
+            console.log(data);
+            if (data.profile.message === 'Not Found') {
+                // Show Alert that user is not Found
+               console.log('User not found!');
+
+            } else {
+                // Show Profile
+                // This will happen through ui class (ui.js)
+                ui.showProfile(data.profile);
+
+                // Show repositories
+                ui.showRepos(data.repos);
+            }
+        });
+
+}
+
+
+
+
 
